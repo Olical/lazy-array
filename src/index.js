41b168dfbe895f7ac107f6db65cfd8ad9a4716a1
Oliver Caldwell <olliec87@gmail.com>
2015-05-25 22:47:57 +0100

    Moved core seq functions into a file

--- src/index.js
+++ src/index.js
@@ -1,53 +1,8 @@
 'use strict';
 
+var _ = require('lodash');
 var LazyArray = require('./LazyArray');
-
-/**
- * Get the first item in an array.
- *
- * @param {*[]} list
- * @return {*} First item.
- */
-function first(list) {
-    if (LazyArray.isLazyArray(list)) {
-        return list.fn()[0];
-    }
-    else {
-        return Array.isArray(list) ? list[0] : null;
-    }
-}
-
-/**
- * Get the tail of the array.
- *
- * @param {*[]} list
- * @return {*[]} Tail.
- */
-function rest(list) {
-    if (LazyArray.isLazyArray(list)) {
-        return list.fn()[1];
-    }
-    else {
-        return Array.isArray(list) ? list.slice(1) : [];
-    }
-}
-
-/**
- * Constructs a new array by prepending the item on the list. When given a
- * LazyArray instance to cons onto, it will return a value/LazyArray pair.
- *
- * @param {*} item
- * @param {*[]} list
- * @return {*[]} Original list with the item at the front.
- */
-function cons(item, list) {
-    if (LazyArray.isLazyArray(list)) {
-        return [item, list];
-    }
-    else {
-        return [item].concat(Array.isArray(list) ? list : []);
-    }
-}
+var seq = require('./seq');
 
 /**
  * Constructs a lazy array instance.
@@ -59,10 +14,7 @@ function create(fn) {
     return new LazyArray(fn);
 }
 
-module.exports = {
-    first: first,
-    rest: rest,
-    cons: cons,
+module.exports = _.assign({
     LazyArray: LazyArray,
     create: create
-};
+}, seq);
---
+++ src/seq.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var LazyArray = require('./LazyArray');
+
+/**
+ * Get the first item in an array.
+ *
+ * @param {*[]} list
+ * @return {*} First item.
+ */
+function first(list) {
+    if (LazyArray.isLazyArray(list)) {
+        return list.fn()[0];
+    }
+    else {
+        return Array.isArray(list) ? list[0] : null;
+    }
+}
+
+/**
+ * Get the tail of the array.
+ *
+ * @param {*[]} list
+ * @return {*[]} Tail.
+ */
+function rest(list) {
+    if (LazyArray.isLazyArray(list)) {
+        return list.fn()[1];
+    }
+    else {
+        return Array.isArray(list) ? list.slice(1) : [];
+    }
+}
+
+/**
+ * Constructs a new array by prepending the item on the list. When given a
+ * LazyArray instance to cons onto, it will return a value/LazyArray pair.
+ *
+ * @param {*} item
+ * @param {*[]} list
+ * @return {*[]} Original list with the item at the front.
+ */
+function cons(item, list) {
+    if (LazyArray.isLazyArray(list)) {
+        return [item, list];
+    }
+    else {
+        return [item].concat(Array.isArray(list) ? list : []);
+    }
+}
+
+module.exports = {
+    first: first,
+    rest: rest,
+    cons: cons
+};
