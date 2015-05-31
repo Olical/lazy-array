466d322916e5159fd7a0ad4277bed79b1944dacb
Oliver Caldwell <olliec87@gmail.com>
2015-05-31 11:39:43 +0100

    Added initial date tests
    Need to try out mapping over a range though, need to build the map
    function first.

---
+++ test/date.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var assert = require('assert');
+var larr = require('..');
+
+// Amount of milliseconds in a day.
+var DAY = 86400000;
+
+/**
+ * Creates an infinite lazy array of all possible times using a given start
+ * date and step timestamp. Providing a negative step will make it go
+ * backwards.
+ *
+ * @param {Date} now
+ * @param {Number} step
+ */
+function dates(now, step) {
+    return larr.create(function () {
+        var next = new Date(now.getTime() + step);
+        return larr.cons(now, dates(next, step));
+    });
+}
+
+describe('date', function () {
+    var start = new Date('1994 Jan 27');
+    var d;
+
+    beforeEach(function () {
+        d = dates(start, DAY);
+    });
+
+    it('should have the start date as the first value', function () {
+        assert.strictEqual(larr.first(d).getTime(), start.getTime());
+    });
+
+    it('should allow me to skip forward some days', function () {
+        var future = larr.nth(d, 3);
+        assert.strictEqual(future.getTime(), new Date('1994 Jan 30').getTime());
+    });
+});
